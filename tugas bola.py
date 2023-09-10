c852c4d81cca0441e08c13b9ec30b2525416b9d1
tnzl-cmd <74844073+tnzl-cmd@users.noreply.github.com>
2023-09-10 20:30:44 +0700

    Add files via upload

---
+++ tugas bola.py
@@ -0,0 +1,12 @@
+nama = input ("masukkan nama : ")
+print ("selamat datang di kalkulator simpel saya")
+print ("hello, " , nama)
+print ("perkenalkan nama saya cindy rosemelinda")
+print ("NIM : 230441100118 ")
+print (input ("ada perlu apa? "))
+a = int (input ("jari-jari bola : "))
+b = 22/7
+c = 4/3
+d = int (a*a*a)
+hasil = int (c*(b*d))
+print ("volume bola tersebut adalah : ", hasil )
